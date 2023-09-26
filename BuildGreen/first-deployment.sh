a289c636733ea849ba7ffd69acde47a5e2bb7ba1
Guillermo Gutierrez <guillermo.gutierrez@rackspace.com>
2023-09-25 18:29:31 -0600

    updates

--- BuildGreen/first-deployment.sh
+++ BuildGreen/first-deployment.sh
@@ -5,20 +5,20 @@ if [ $checkDeployments -eq 0 ]
 then
     echo "[INFO] No previus deployments found in $EKS_NAMESPACE namespace"
     echo "[INFO] Start with the first Deployment"
-    cat BuildGreen/k8s/app-namespace.yml
-    kubectl apply -f BuildGreen/k8s/app-namespace.yml
-    cat BuildGreen/k8s/app-serviceaccount.yml
-    kubectl apply -f BuildGreen/k8s/app-serviceaccount.yml
-    cat BuildGreen/k8s/app-deployment-green.yml
-    kubectl apply -f BuildGreen/k8s/app-deployment-green.yml
-    cat BuildGreen/k8s/app-deployment-blue.yml
-    kubectl apply -f BuildGreen/k8s/app-deployment-blue.yml
-    cat BuildGreen/k8s/app-service-green.yml
-    kubectl apply -f BuildGreen/k8s/app-service-green.yml
-    cat BuildGreen/k8s/app-service-blue.yml
-    kubectl apply -f BuildGreen/k8s/app-service-blue.yml
-    cat BuildGreen/k8s/app-ingress-blue.yml
-    kubectl apply -f BuildGreen/k8s/app-ingress-blue.yml
-    cat BuildGreen/k8s/app-ingress-green.yml
-    kubectl apply -f BuildGreen/k8s/app-ingress-green.yml
+    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-namespace.yml
+    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-namespace.yml
+    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-serviceaccount.yml
+    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-serviceaccount.yml
+    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-deployment-green.yml
+    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-deployment-green.yml
+    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-deployment-blue.yml
+    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-deployment-blue.yml
+    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-service-green.yml
+    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-service-green.yml
+    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-service-blue.yml
+    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-service-blue.yml
+    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-ingress-blue.yml
+    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-ingress-blue.yml
+    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-ingress-green.yml
+    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-ingress-green.yml
 fi
