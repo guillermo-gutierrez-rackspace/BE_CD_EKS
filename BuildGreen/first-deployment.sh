3cedb2e9c79871196874c1708e54258ddb2eb4d7
Guillermo Gutierrez <guillermo.gutierrez@rackspace.com>
2023-09-27 13:34:12 -0600

    updates

--- BuildGreen/first-deployment.sh
+++ BuildGreen/first-deployment.sh
@@ -5,20 +5,20 @@ if [ $checkDeployments -eq 0 ]
 then
     echo "[INFO] No previus deployments found in $EKS_NAMESPACE namespace"
     echo "[INFO] Start with the first Deployment"
-    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-namespace.yaml
-    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-namespace.yaml
-    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-serviceaccount.yaml
-    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-serviceaccount.yaml
-    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-deployment-green.yaml
-    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-deployment-green.yaml
-    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-deployment-blue.yaml
-    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-deployment-blue.yaml
-    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-service-green.yaml
-    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-service-green.yaml
-    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-service-blue.yaml
-    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-service-blue.yaml
-    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-ingress-blue.yaml
-    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-ingress-blue.yaml
-    cat              $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-ingress-green.yaml
-    kubectl apply -f $CODEBUILD_SRC_DIR_AppSourceOutput/k8s/app-ingress-green.yaml
+    cat              BuildGreen/k8s/app-namespace.yaml
+    kubectl apply -f BuildGreen/k8s/app-namespace.yaml
+    cat              BuildGreen/k8s/app-serviceaccount.yaml
+    kubectl apply -f BuildGreen/k8s/app-serviceaccount.yaml
+    cat              BuildGreen/k8s/app-deployment-green.yaml
+    kubectl apply -f BuildGreen/k8s/app-deployment-green.yaml
+    cat              BuildGreen/k8s/app-deployment-blue.yaml
+    kubectl apply -f BuildGreen/k8s/app-deployment-blue.yaml
+    cat              BuildGreen/k8s/app-service-green.yaml
+    kubectl apply -f BuildGreen/k8s/app-service-green.yaml
+    cat              BuildGreen/k8s/app-service-blue.yaml
+    kubectl apply -f BuildGreen/k8s/app-service-blue.yaml
+    cat              BuildGreen/k8s/app-ingress-blue.yaml
+    kubectl apply -f BuildGreen/k8s/app-ingress-blue.yaml
+    cat              BuildGreen/k8s/app-ingress-green.yaml
+    kubectl apply -f BuildGreen/k8s/app-ingress-green.yaml
 fi
